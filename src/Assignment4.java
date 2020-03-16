f3287feb4e6284e0b22b62602e7792e93e256875
Justi <Justi@Alpha-RX>
2020-03-16 15:27:12 -0400

    updated to have basic information

--- src/Assignment4.java
+++ src/Assignment4.java
@@ -8,15 +8,43 @@ import java.util.*;
  *
  */
 public class Assignment4 {
-	
+
 	public static <T extends Comparable<? super T>> void recursiveSelectionSort (T[] theArray, int n)
 	{
-		return;
+		if(n < 1)
+		{
+			System.out.println("The array is invalid");
+		}
+		else
+		{
+			int max_ind = max(theArray); // index for the largest;
+			System.out.println(max_ind);
+		}
 	}
 
 	public static <T extends Comparable<? super T>> void recursiveBubbleSort (T[] theArray, int n)
 	{
 		return;
+	}
+
+	/**
+	 * finds the max value in a given array
+	 * @param arr: the array
+	 * @return the index for the largest number
+	 */
+	public static <T extends Comparable<? super T>> int max(T[] theArray)
+	{
+		int maxVal = (int) theArray[0];
+		int j = -1;
+		for(int i = 0 ; i < theArray.length; i++)
+		{
+			if((int) theArray[i] > maxVal)
+			{
+				maxVal = (int) theArray[i];
+				j = i;
+			}
+		}
+		return j;
 	}
 	/**
 	 * 
@@ -30,7 +58,8 @@ public class Assignment4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Integer [] arr = {29, 10, 14, 37, 13};
+		recursiveSelectionSort(arr, 5);
 	}
 
 }
